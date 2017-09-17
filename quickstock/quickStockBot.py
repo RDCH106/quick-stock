d43f0f02341f60c06005a3aa00d476318462416e
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-09-17 13:43:20 +0200

    Fixed message handler

--- quickstock/quickStockBot.py
+++ quickstock/quickStockBot.py
@@ -144,7 +144,7 @@ def main():
     dp.add_handler(CommandHandler("deleteItem", deleteItem))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(MessageHandler([Filters.text], echo))
 
     # log all errors
     dp.add_error_handler(error)
