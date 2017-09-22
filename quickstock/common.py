2ea2985a88f9e16906d81889226f86d28f830db7
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-09-22 15:29:00 +0200

    Different text style for item with amount 0 and 1

--- quickstock/common.py
+++ quickstock/common.py
@@ -10,7 +10,13 @@ def stringifySotckToList(collection):
 def stringifyItemToList(collection):
     list = ""
     for item in collection:
-        list = list + '{:03d}'.format(item.id) + "  " + item.name + " " + str(item.amount) + "\n"
+        if item.amount == 0:
+            list = list + '**{:03d}'.format(item.id) + "  " + item.name + " " + str(item.amount) + "**\n"
+        else:
+            if item.amount == 1:
+                list = list + '__{:03d}'.format(item.id) + "  " + item.name + " " + str(item.amount) + "__\n"
+            else:
+                list = list + '{:03d}'.format(item.id) + "  " + item.name + " " + str(item.amount) + "\n"
     return list
 
 def validID(id, collection):
