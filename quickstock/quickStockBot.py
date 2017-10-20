31b6e31407829fee789f40a3cc51a40cbfd5b974
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-10-20 14:46:16 +0200

    Added + and - operators to updateItemAmount

--- quickstock/quickStockBot.py
+++ quickstock/quickStockBot.py
@@ -98,9 +98,17 @@ def updateItem(bot, update):
     if item is not None and common.validID(item.stock_id, stocks):
         if update.message.text.split(" ", 2)[0] == "/updateItemAmount":
             items = qs.getStockItems(update.message.text.split(" ", 1)[1])
-            qs.updateItem(id_item=update.message.text.split(" ", 2)[1], amount=update.message.text.split(" ", 2)[2])
-            update.message.reply_text("'%s' --> %s updated!" % (item.name,
-                                                                update.message.text.split(" ", 2)[2]))
+            if update.message.text.split(" ", 2)[2][0] == "+" or update.message.text.split(" ", 2)[2][0] == "-":
+                amount = qs.getItem(update.message.text.split(" ", 2)[1]).amount
+                qs.updateItem(id_item=update.message.text.split(" ", 2)[1],
+                              amount=amount + int(update.message.text.split(" ", 2)[2]))
+                update.message.reply_text("'%s' --> %s updated!" % (item.name,
+                                                                    str(amount)+update.message.text.split(" ", 2)[2]))
+            else:
+                qs.updateItem(id_item=update.message.text.split(" ", 2)[1], amount=update.message.text.split(" ", 2)[2])
+                update.message.reply_text("'%s' --> %s updated!" % (item.name,
+                                                                    update.message.text.split(" ", 2)[2]))
+
         if update.message.text.split(" ", 2)[0] == "/updateItemName":
             items = qs.getStockItems(update.message.text.split(" ", 1)[1])
             qs.updateItem(id_item=update.message.text.split(" ", 2)[1], name=update.message.text.split(" ", 2)[2])
